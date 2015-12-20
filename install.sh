f27b76f1ba029c34d25ed5883b3d9eb6f2a5ea5b
root <root@cos01.(none)>
2015-12-20 13:45:10 +0800

    dev

---
+++ install.sh
@@ -0,0 +1,7 @@
+#yum install postgresql-devel*  
+#wget https://github.com/bluedazzle/django-angularjs-blog.git
+pip install -r requirements.txt
+python manage.py syncdb
+#python manage.py test
+#6、run test server:
+#python manage.py runserver 10.0.0.8000
